fd0dc722d0cd6c26c0b1567b41f75cf9770530ce
Natthawat-p <115424398+Natthawat-p@users.noreply.github.com>
2022-11-12 16:56:45 +0700

    Initial commit

---
+++ Exercise4_Natthawat_P.py
@@ -0,0 +1,9 @@
+FE = float(input("Foundation English : "))
+genBusiness = float(input("General Business: "))
+IntroComputer = float(input("Introduction to Computer Systems : "))
+ComputerPro = float(input("Computer Programming : "))
+
+print("--- Your Score --- ""\nFoundation English :" , FE ,
+      "\nGeneral Business :" , genBusiness,
+      "\nIntroduction to Computer Systems : " , IntroComputer  ,
+      "\nComputer Programming :  " ,ComputerPro  )
